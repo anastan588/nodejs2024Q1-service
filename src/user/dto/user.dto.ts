2ff1cf651ba68ac40812147f722c5ed149da39f5
Anastasiya Andronova <anastasiya5.88@mail.ru>
2024-03-10 14:30:38 +0300

    feat: edit, user interface: creation, recieving, updating, deleting, start track

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,27 @@
+import { Exclude } from 'class-transformer';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class IDUserParametr {
+  @IsUUID()
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+}
+
+export class NewUserParametr {
+  @IsString()
+  @IsNotEmpty()
+  login: string;
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class UpdateUserParametr extends NewUserParametr {
+  @IsString()
+  @IsNotEmpty()
+  oldPassword: string;
+  @IsString()
+  @IsNotEmpty()
+  newPassword: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,51 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { User } from 'src/database/database.types';
+import { NewUserParametr, UpdateUserParametr } from './dto/user.dto';
+import { v4 } from 'uuid';
+
+@Injectable()
+export class UserService {
+  constructor(private database: DatabaseService) {}
+
+  getAllUsers() {
+    return this.database.users;
+  }
+  getUserByID(id: string): User {
+    const user = this.database.users.find((user) => user.id === id);
+    return user;
+  }
+
+  createUser(createUser: NewUserParametr): User {
+    const newUser: User = {
+      id: v4(),
+      login: createUser.login,
+      password: createUser.password,
+      createdAt: new Date().getTime(),
+      updatedAt: new Date().getTime(),
+      version: 1,
+    };
+    this.database.users.push(newUser);
+    return newUser;
+  }
+
+  deleteUser(id: string) {
+    const userID = this.database.users.findIndex((user) => user.id === id);
+    this.database.users.splice(userID, 1);
+    return userID;
+  }
+
+  updateUserPassword(id: string, updateUser: UpdateUserParametr): User {
+    const user = this.database.users.find((user) => user.id === id);
+    if (!user) {
+      throw new HttpException('User not found', 404);
+    }
+    if (user.password !== updateUser.oldPassword) {
+      throw new HttpException('Wrong old password', HttpStatus.FORBIDDEN);
+    }
+    user.version += 1;
+    user.updatedAt = new Date().getTime();
+    user.password = updateUser.newPassword;
+    return user;
+  }
+}
